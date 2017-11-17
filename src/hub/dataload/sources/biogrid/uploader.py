27f1e6a29f87105e35232677d9daebad1a159644
Greg Taylor <greg@spritzr.com>
2017-11-17 06:48:05 -0800

    Changes to biogrid uploaded to properly use ZipFile.

--- src/hub/dataload/sources/biogrid/uploader.py
+++ src/hub/dataload/sources/biogrid/uploader.py
@@ -13,9 +13,18 @@ class BiogridUploader(uploader.BaseSourceUploader):
     # main_source = "biogrid"
     name = "biogrid"
     collection_name = "biogrid"
+
+    # file information
     zip_file_name = "BIOGRID-ALL-3.4.154.tab2.zip"
+    tab_file = "BIOGRID-ALL-3.4.154.tab2.txt"
 
     def load_data(self, data_folder):
         consensus_file = os.path.join(data_folder, self.zip_file_name)
+
         self.logger.info("Load biogrid data from file '%s'" % consensus_file)
-        return parse_biogrid(ZipFile.open(consensus_file, mode='rt'))
+
+        # Open the first file in the zip file - assuming that it is the data file
+        zip = ZipFile(self.consensus_file)
+        tab_file = zip.open(zip.namelist()[0], mode='r')
+
+        return parse_biogrid(tab_file)

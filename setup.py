d7f4d7e0b0a6e3648bd894b435fc282c5f25fb6b
Greg Taylor <greg.k.taylor@gmail.com>
2017-12-15 21:22:00 +0000

    Package Management:  establish biointeract python package with refactor recommended by setuptools.

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+setup.py -- package installation for biointeract
+"""
+from setuptools import setup
+
+
+setup(name='biointeract',
+      version='0.1.4',
+      description='A biothings api service for interaction data.',
+      url='https://github.com/biothings/biothings.interactions',
+      author='Greg Taylor',
+      author_email='greg.k.taylor@gmail.com',
+      license='Apache License Version 2.0',
+      packages=['biointeract'],
+      install_requires=[
+          'biopython',
+      ],
+      zip_safe=False)
+
